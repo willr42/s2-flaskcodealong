32113c50c030cf0e25b36b25dbe61d97e9414221
Will Robertson <williamdrobertson42@gmail.com>
2023-01-28 10:49:36 +1100

    Write book model

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# Setup
 app = Flask(__name__)
 app.config.from_object("config.app_config")
+
+# Models
+db = SQLAlchemy(app)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    book_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    genre = db.Column(db.String())
+    length = db.Column(db.Integer)
+    year = db.Column(db.Integer)
