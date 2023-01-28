9d75291d8989267edaf54e6f55345ec06b57dd57
Will Robertson <williamdrobertson42@gmail.com>
2023-01-28 13:02:22 +1100

    Add routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import click
@@ -48,10 +48,10 @@ def seed_db():
 
     # Create book objects
     book1 = Book(
-        title="Harry Potter & the Philosopher's Stone",
+        title="The Left Hand of Darkness",
         genre="fantasy",
-        length=400,
-        year=1999,
+        length=286,
+        year=1969,
     )
     book2 = Book(
         title="Dune",
@@ -61,3 +61,43 @@ def seed_db():
     )
     db.session.add_all([book1, book2])
     db.session.commit()
+
+
+@app.route("/books", methods=["GET"])
+def get_books():
+    book_list = Book.query.all()
+    return books_schema.dump(book_list)
+
+
+@app.route("/books/<int:id>", methods=["GET"])
+def get_book(id):
+    book = Book.query.get(id)
+    return book_schema.dump(book)
+
+
+@app.route("/books", methods=["POST"])
+def add_book():
+    # load the fields from the request JSON
+    book_fields = book_schema.load(request.json)
+
+    # Create new empty book
+    new_book = Book()
+
+    # dynamically loop over the keys
+    for field in book_fields:
+        setattr(new_book, field, book_fields[field])
+
+    # or you can just loop over the keys manually as done in class
+    #
+    # new_book = Book(
+    #     title=book_fields["title"],
+    #     year=book_fields["year"],
+    #     genre=book_fields["genre"],
+    #     length=book_fields["length"],
+    # )
+
+    # commit to DB
+    db.session.add(new_book)
+    db.session.commit()
+
+    return book_schema.dump(new_book)
